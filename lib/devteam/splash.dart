514d469d860eea369e21e3617ab92fdddbfb2c8e
Gianluca084 <ceramellagianluca@gmail.com>
2025-03-05 17:08:13 +0100

    Update splash.dart

--- lib/devteam/splash.dart
+++ lib/devteam/splash.dart
@@ -5,7 +5,7 @@ import 'package:prima_applicazione/MyButton.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:prima_applicazione/login/loginpage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
+import 'package:flutter/src/painting/gradient.dart';
 class Splash extends StatelessWidget {
   const Splash({super.key});
 
@@ -30,86 +30,36 @@ class MySplash extends StatefulWidget {
 }
 
 class _MySplashState extends State<MySplash> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    if (_counter>0) {
-      setState(() {
-        _counter--;
-      });
-    } else {
-      print("Errore");
-    }
-  }
-
-  void _resetCountet() {
-    setState(() {
-      _counter=0;
-    });
-
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-          systemOverlayStyle: SystemUiOverlayStyle.light,
-
-        ),
-        body: Center(
+      body: Container(
+          width: double.maxFinite,
+          height: double.maxFinite,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment. topCenter,
+              end: Alignment. bottomCenter,
+              colors: [Colors.blue.shade900, Color.fromARGB(140, 0, 0, 172)],
+            ),
+          ),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-                style: TextStyle(
-                    color: Colors.red
-                ),
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  MyButton(
-                      onPressed: _decrementCounter,
-                      child: const Icon(Icons.accessibility)
-                  ),
-                  const SizedBox(width: 20),
-                  MyButton(
-                      onPressed: _resetCountet,
-                      child: const Icon(Icons.lock_reset)
-                  ),
-                  const SizedBox(width: 20),
-                  MyButton(
-                    onPressed: _incrementCounter,
-                    child: const Icon(Icons.add),
-                  ),
-                  const SizedBox(width: 20),
-                  MyButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const LoginPage())
-                        );
-                      },
-                      child: const Icon(Icons.edit)
-                  )
-                ],
-              )
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text('Superior University',
+              style: TextStyle(color: Colors.white, fontSize: 30, font),),
+              Text('e-Learning Platform',
+              style: TextStyle(color: Colors.white, fontSize: 20)),
+              SizedBox(height: 450),
+              Text('Copyright © 2022 Superior University',
+              style: TextStyle(color: Colors.white),),
+              Text('All Right Reserved',
+              style: TextStyle(color:Colors.white),),
+              SizedBox(height: 50)
             ],
           ),
-        )
+    )
     );
   }
 }
