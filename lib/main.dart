e2c5325f82351ace1aae70824f721dd31cf9ef65
Mario Monaco <andoma93@gmail.com>
2025-03-05 16:23:00 +0100

    Fixes

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,12 @@ import 'package:flutter/services.dart';
 import 'package:prima_applicazione/MyButton.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:prima_applicazione/login/loginpage.dart';
+import 'devteam/profilo.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
   runApp(const MyApp());
 }
 
@@ -26,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'QuickCare'),
+      home: const ProfiloPage(title: 'Profilo'),
     );
   }
 }
