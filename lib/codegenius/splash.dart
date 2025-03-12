d1f3947942319868470f0f109f72b9b8e4dca273
giudlui <Luigiorlando2007@gmail.com>
2025-03-12 15:52:57 +0100

    Update splash.dart

--- lib/codegenius/splash.dart
+++ lib/codegenius/splash.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:prima_applicazione/MyButton.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:prima_applicazione/login/loginpage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class Splash extends StatefulWidget {
